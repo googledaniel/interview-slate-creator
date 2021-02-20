ca5aeaf59965ace1747c399476b76ee4ea87dbdb
Daniel <dcloudt@google.com>
2021-02-20 10:06:22 -0800

    another heroku production bug fix

--- models/questionModel.js
+++ models/questionModel.js
@@ -11,7 +11,55 @@ const Question = {
 		'Make an API request from omdb.',
 		'Use media queries in css to create a reactive webpage.'
 	],
+	DS3: [
+		'Distributed systems Level 3 (Question A).',
+		'Distributed systems Level 3 (Question B).'
+	],
 	culture3: [
+		`Describe the personality of a manager who you’d never want to work for.`,
+		`How would your last manager describe you? What about your past colleagues?`
+	],
+	coding4: [
+		`Given an integer array, move all elements that are 0 to the left while maintaining the order of other elements in the array. The array has to be modified in-placeA`,
+		`You are given an array (list) of interval pairs as input where each interval has a start and end timestamp. The input array is sorted by starting timestamps. You are required to merge overlapping intervals and return a new output array.`,
+		`Convert a binary tree to a doubly linked list so that the order of the doubly linked list is the same as an in-order traversal of the binary tree.`,
+		`Reverse the order of words in a given sentence (an array of characters).`
+	],
+	FETech4: [
+		'Create a pet store homepage with Sass.',
+		'Create a navbar using react router.',
+		'Make an API request from omdb.',
+		'Use media queries in css to create a reactive webpage.'
+	],
+	DS4: [
+		'Distributed systems Level 4 (Question A).',
+		'Distributed systems Level 4 (Question B).'
+	],
+	culture4: [
+		`Describe the personality of a manager who you’d never want to work for.`,
+		`How would your last manager describe you? What about your past colleagues?`
+	],
+	coding5: [
+		`Given an integer array, move all elements that are 0 to the left while maintaining the order of other elements in the array. The array has to be modified in-placeA`,
+		`You are given an array (list) of interval pairs as input where each interval has a start and end timestamp. The input array is sorted by starting timestamps. You are required to merge overlapping intervals and return a new output array.`,
+		`Convert a binary tree to a doubly linked list so that the order of the doubly linked list is the same as an in-order traversal of the binary tree.`,
+		`Reverse the order of words in a given sentence (an array of characters).`
+	],
+	FETech5: [
+		'Create a pet store homepage with Sass.',
+		'Create a navbar using react router.',
+		'Make an API request from omdb.',
+		'Use media queries in css to create a reactive webpage.'
+	],
+	DS5: [
+		'Distributed systems Level 5 (Question A).',
+		'Distributed systems Level 5 (Question B).'
+	],
+	SD5: [
+		'System Design Level 5 (Question A).',
+		'System Design Level 5 (Question B).'
+	],
+	culture5: [
 		`Describe the personality of a manager who you’d never want to work for.`,
 		`How would your last manager describe you? What about your past colleagues?`
 	]
--- models/rubricModel.js
+++ models/rubricModel.js
@@ -1,7 +1,18 @@
 const rubricModel = {
 	3: {
 		FE: ['coding3', 'coding3', 'FETech3', 'FETech3', 'culture3'],
-		BE: ['coding3', 'coding3', 'coding3', 'coding3', 'culture3']
+		BE: ['coding3', 'coding3', 'coding3', 'coding3', 'culture3'],
+		Infra: ['coding3', 'coding3', 'DS3', 'DS3', 'culture3']
+	},
+	4: {
+		FE: ['coding4', 'coding4', 'FETech4', 'FETech4', 'culture4'],
+		BE: ['coding4', 'coding4', 'coding4', 'coding4', 'culture4'],
+		Infra: ['coding4', 'coding4', 'DS4', 'DS4', 'culture4']
+	},
+	5: {
+		FE: ['coding5', 'SD5', 'FETech5', 'FETech5', 'culture5'],
+		BE: ['coding5', 'coding5', 'coding5', 'SD5', 'culture5'],
+		Infra: ['coding5', 'coding5', 'DS5', 'SD5', 'culture5']
 	}
 };
 module.exports = rubricModel;
